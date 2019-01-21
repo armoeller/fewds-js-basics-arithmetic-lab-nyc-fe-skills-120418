9ba56d946c8b607408348cf82c79da5b64a31ea1
Andy Moeller <ar.moeller1@gmail.com>
2019-01-21 17:50:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ let newID = 1000000000 + oldID
 
 let ageIsValid = Number.isInteger(currentAge);
 
-let randomNumber = Math.random(20)
+let randomNumber = (Math.random() * 20) + 1;
 
 let randomInteger = Math.floor(randomNumber)
 
-const randomUserId = randomNumber + 1000000000
+const randomUserId = randomNumber + 1000000000;
