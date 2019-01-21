049fcd35f06e47c5ac4eca1af06761ff8fcb89af
Andy Moeller <ar.moeller1@gmail.com>
2019-01-21 17:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ let randomNumber = Math.random(20)
 
 let randomInteger = Math.floor(randomNumber)
 
-let randomUserId = randomInteger * 1000000001;
+let randomUserId = (randomInteger * 1) + 1000000000;
